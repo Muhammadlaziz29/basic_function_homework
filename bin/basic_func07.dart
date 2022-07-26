61b91acf0c2df59d4215c46b3d42cd88c15a4e08
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-07-26 12:10:22 +0500

    hello

--- bin/basic_func07.dart
+++ bin/basic_func07.dart
@@ -1,6 +1,13 @@
 //  Create a function called func.
 
+int func(int a) {
+    return a;
+}
+
 //  Create function arguments a.
 
 //  Return the value a.
-void main() {}
+void main() {
+    
+    print(func(5));
+}
