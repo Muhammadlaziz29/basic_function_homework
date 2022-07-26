754217afadf2acf92c543f835144f467a3cdd301
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-07-26 12:18:15 +0500

    hello

--- bin/basic_func10.dart
+++ bin/basic_func10.dart
@@ -1,6 +1,12 @@
 //  Create a function called func.
 
+int func(int a) {
+    return a * (-1);
+}
+
 //  Create function arguments a.
 
 //  Return the negative value of a.
-void main() {}
+void main() {
+    print(func(5));
+}
